77a3e1255770820b8a8cffbe9426c375d47341c8
csikszabi04 <alexszabi04@gmail.com>
2024-03-19 15:10:11 +0100

    Kattint

--- src/main/java/com/example/dobozok/HelloController.java
+++ src/main/java/com/example/dobozok/HelloController.java
@@ -16,17 +16,21 @@ public class HelloController {
     @FXML private ImageView ivArrow;
 
     private Label[][] list = new Label[10][15];
+    private char[][] clist = new char[10][15];
     AnimationTimer timer = null;
 
     int dbopen = 0;
     int dbclose = 0;
     long step = 0;
     int fok = 90;
+    int igaz = 0;
+    int nem = 0;
 
     public void initialize(){
         for(int s=0; s<10; s++) for(int o=0; o<15; o++){
             int ss=s, oo=o;
             list[s][o] = new Label("");
+            clist[s][o] = 'X';
             list[s][o].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("null.png"))));
             list[s][o].setTranslateX(10+o*64);
             list[s][o].setTranslateY(10+s*64);
@@ -34,6 +38,11 @@ public class HelloController {
             list[s][o].setOnMouseExited(e -> list[ss][oo].setStyle("-fx-background-color: white"));
             list[s][o].setOnMouseClicked(e -> click(ss,oo));
             pnPane.getChildren().add(list[s][o]);
+            if(clist[s][o] == 'X'){
+                igaz = 1;
+            }else if(clist[s][o] == 'O'){
+                igaz = 0;
+            }
         }
         timer = new AnimationTimer() {
             @Override
@@ -61,6 +70,7 @@ public class HelloController {
             int rand = (int)(Math.random()*10);
             while(rand != 0){
                 list[s][o].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("box.png"))));
+                clist[s][o] = 'O';
                 s--; rand--; dbclose++;
             }
         }
@@ -70,6 +80,7 @@ public class HelloController {
         for(int s=0; s<10; s++) for(int o=0; o<15; o++){
             dbopen=0; dbclose=0;
             list[s][o].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("null.png"))));
+            clist[s][o] = 'X';
         }
         add();
         db();
@@ -77,7 +88,11 @@ public class HelloController {
 
     private void click(int ss, int oo){
         dbclose--; dbopen++;
-        list[ss][oo].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("boxopen.png"))));
+            if(igaz == 0){
+                list[ss][oo].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("null.png"))));
+            }else if(igaz == 1){
+                list[ss][oo].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("boxopen.png"))));
+            }
         db();
     }
 
